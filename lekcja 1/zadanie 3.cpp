43cb187c1f31688cf06010a390ca6768e8b9f8b1
Gabriel Dziedzic (Uczeń) <gdziedzic818@technikumkreatywne.pl>
2024-11-21 09:01:36 +0100

    kuba jest gruby

---
+++ lekcja 1/zadanie 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+int main() {
+ int liczba;
+ 
+ std::cout << "Podaj liczbe calkowita: ";
+ std::cin >> liczba;
+ 
+ if (liczba % 2 == 0) {
+ std::cout << "Liczba " << liczba << " jest parzysta." << std::endl;
+ } else {
+ std::cout << "Liczba " << liczba << " jest nieparzysta." << std::endl;
+ }
+ 
+ if (liczba % 3 == 0) {
+ std::cout << "Liczba " << liczba << " jest podzielna przez 3." << std::endl;
+ } else {
+ std::cout << "Liczba " << liczba << " nie jest podzielna przez 3." << std::endl;
+ }
+ 
+ return0;
+}
+ 
