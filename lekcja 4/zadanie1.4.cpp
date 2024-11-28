17edba0f56dd8c0ea2f47978fceedc7f3205dda9
Gabriel Dziedzic (Uczeń) <gdziedzic818@technikumkreatywne.pl>
2024-11-28 09:06:10 +0100

    cos

---
+++ lekcja 4/zadanie1.4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N;
+ 
+    cout << "Podaj liczbe N: ";
+    cin >> N;
+ 
+    int i = 2;
+    cout << "Liczby parzyste od 2 do " << N << ":\n";
+    while (i <= N) {
+        cout << i << endl;
+        i += 2;
+    }
+ 
+    return 0;
+}
+ 
